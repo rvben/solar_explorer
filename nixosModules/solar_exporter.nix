4f803bf641349e7568c684a61bcfb4c0fd04a3f1
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2024-08-23 21:17:29 +0200

    Try fix

--- nixosModules/solar_exporter.nix
+++ nixosModules/solar_exporter.nix
@@ -1,45 +1,41 @@
-{ lib, pkgs, config, ... }:
+{ config, lib, pkgs, solar_exporter, ... }:
 
-with lib;                      
+with lib;
 let
   cfg = config.services.solar_exporter;
 in {
   options = {
     services.solar_exporter = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = "Enable the Solar Exporter service.";
-      };
+      enable = mkEnableOption "Solar Exporter service";
 
-      user = lib.mkOption {
-        type = lib.types.str;
-        default = "solar_exporter_user";
+      user = mkOption {
+        type = types.str;
+        default = "solar_exporter";
         description = "User to run the Solar Exporter service.";
       };
 
-      group = lib.mkOption {
-        type = lib.types.str;
-        default = "solar_exporter_group";
+      group = mkOption {
+        type = types.str;
+        default = "solar_exporter";
         description = "Group to run the Solar Exporter service.";
       };
 
-      configFile = lib.mkOption {
-        type = lib.types.path;
+      configFile = mkOption {
+        type = types.path;
         default = "/etc/solar_exporter/config.yml";
         description = "Path to the Solar Exporter configuration file.";
       };
     };
   };
 
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
     systemd.services.solar_exporter = {
       description = "Solar Exporter Service";
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
 
       serviceConfig = {
-        ExecStart = "${pkgs.solar_exporter}/bin/solar_exporter --config ${escapeShellArg cfg.configFile}";
+        ExecStart = "${solar_exporter}/bin/solar_exporter --config ${escapeShellArg cfg.configFile}";
         Restart = "always";
         User = cfg.user;
         Group = cfg.group;
